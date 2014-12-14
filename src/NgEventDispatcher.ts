8d5a1cdb95952d6be0aa7dae69677f94cc39df46
Piotr Gąsiorowski <gasiorowski.piotr@gmail.com>
2014-12-14 23:15:28 +0100

    Angular-specific behavior on EventDispatcher.

---
+++ src/NgEventDispatcher.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../bower_components/base-event-dispatcher/dist/dts/eventDispatcher.d.ts"/>
+/// <reference path="../bower_components/DefinitelyTyped/angularjs/angular.d.ts"/>
+
+module evilduck {
+    export module angular {
+        export class NgEventDispatcher extends EventDispatcher {
+
+            public ngOn($scope: ng.IScope, event: string, handler: (any: any) => any, tag?: string): void {
+
+                var subsInfo = this.on(event, handler, tag);
+                if (subsInfo) {
+
+                    $scope.$on('destroy', () => {
+                        subsInfo.destroy();
+                    });
+                }
+
+            }
+
+        }    
+    }
+} 
--- src/z_main.ts
+++ src/z_main.ts
@@ -7,4 +7,4 @@ angular
     .module('evilduck.eventDispatcher', [])
     .service('collWrapper', () => new evilduck.underscore.Underscore())
     .service('qWrapper', ['$q', 'collWrapper', ($q: ng.IQService, collWrapper: evilduck.underscore.Underscore) => new evilduck.angular.QService($q, collWrapper)])
-    .service('eventDispatcher', ['qWrapper', 'collWrapper', (q: evilduck.angular.QService, c: evilduck.underscore.Underscore) => new evilduck.EventDispatcher(q, c)]);
+    .service('eventDispatcher', ['qWrapper', 'collWrapper', (q: evilduck.angular.QService, c: evilduck.underscore.Underscore) => new evilduck.angular.NgEventDispatcher(q, c)]);
